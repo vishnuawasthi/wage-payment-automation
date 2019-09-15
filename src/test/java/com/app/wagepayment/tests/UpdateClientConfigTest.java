92d98dfe6d1d43ce4bedffad0ba733986ef40dc8
arti20 <arti.goswami02@gmail.com>
2019-09-15 22:06:28 +0530

    Added Update client config test case class.

---
+++ src/test/java/com/app/wagepayment/tests/UpdateClientConfigTest.java
@@ -0,0 +1,18 @@
+package com.app.wagepayment.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class UpdateClientConfigTest {
+
+	@BeforeTest
+	public void init() {
+
+	}
+
+	@Test
+	public void testDemo() {
+		Assert.assertEquals(true, true);
+	}
+}
